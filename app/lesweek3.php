64a1b8b3212478a99a4d5a54b7141704b6f83e58
adruijter <adruijter@gmail.com>
2025-10-02 15:56:54 +0200

    tussen commit

--- app/lesweek3.php
+++ app/lesweek3.php
@@ -17,6 +17,72 @@
 
         $snack = "bitterbal";
         echo "<p>Mijn favoriete snack is $snack</p>";
+        echo '<p>Mijn favoriete snack is $snack</p>';
+
+        // Integer variabele mag positief of negatief zijn maar wel een geheel getal
+        $leeftijd = 57;
+        echo "<p>Mijn leeftijd is $leeftijd</p>";
+
+        // Float is een positief of negatief kommagetal. De komma schrijf je met een punt
+        $prijs = 4.65;
+        echo "Mijn heerlijke turkse pizza was &euro; $prijs";
+
+        // Boolean variabele kan twee waarden aannemen true of false, 1 of 0
+        $wordIkMiljonairLater = false;
+
+        if ($wordIkMiljonairLater) {
+            echo "<p>Ik wordt miljonair later</p>";
+        } else {
+            echo "<p>Ik wordt gelukkig</p>";
+        }
+
+
+        /**
+         * Een indexed array is een variabele waarin je meerdere waarden kunt opslaan. 
+         * Iedere waarde in het array heeft een getalletje aan zich gekoppeld 
+         */
+        
+        $repen = [
+            'mars',
+            'Twix',
+            'KitKat',
+            'Bounty',
+            'Milky way',
+            'Snickers',
+            'Galaxy'
+        ];
+
+        // Zet op het scherm de zin Mijn favoriete reep is een Twix
+        echo "<p>Mijn favoriet reep is $repen[6]</p>";
+
+
+        $games = [
+            'Tomb Raider',
+            'Kings Valley',
+            'Donkey Kong',
+            'War of the world',
+            'Texmex'
+        ];
+
+        echo "<p id='rood'>Mijn favoriete games</p>";
+        echo "<p>====================</p>";
+        echo "<ol>";
+        echo "<li>$games[0]</li>";
+        echo "<li>$games[1]</li>";
+        echo "<li>$games[2]</li>";
+        echo "<li>$games[3]</li>";
+        echo "<li>$games[4]</li>";
+        echo "</ol>";
+
+
+        
+
+
+
+
+
+
+
     ?>
 
 </body>
